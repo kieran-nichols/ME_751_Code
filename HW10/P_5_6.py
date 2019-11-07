f0f50e5e7d12ac66852e72c89b20951480ae28c2
knichols4 <knichols4@wisc.edu>
2019-11-07 00:12:25 -0600

    finished time integration for finite vol and finite diff for hw10

--- HW10/P_5_6.py
+++ HW10/P_5_6.py
@@ -1,20 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-phi_d_steady = 0 # at steady state and is a function of space where phi = phi(x)
-Delta = 0
 u = 1 # u(x,t)
-phi_bar = 0
 D = 1 # D(x,t)
-Delta_phi_bar = 0
 s = 1 # s(x,t)
-x0 = 0
 
-# Laplace: u=0 and s!=0
-# Diffusion u=0 and s=0
-
-n = 200
-m = 1000
+n = 100
+m = 500
 itr = n + 1
 itr_m = m + 1
 
@@ -22,21 +14,14 @@ x = np.linspace(0., 1., num=itr)
 time = np.linspace(0., 5., num=itr_m)
 dt = time[1]
 phi = np.zeros([itr])
-#A = np.zeros([itr,itr,itr])
-#b = np.zeros([itr,itr])
 A = np.zeros([itr,itr])
 b = np.zeros([itr])
 dx = x[1]
-#phi = np.linspace(0., 1., num=itr) # want to solve for phi using linear equation A*phi = b
-# what is A and b?
 
-#s = phi_d + np.matmul(Delta,(np.matmul(u,phi_bar) - Delta_phi_bar))
 A[0,0] = 1; b[0] = 0 #A[0,1] = - 2*D/dx**2; A[0,2] = - u/(2*dx) - D/dx**2 ;
 A[n,n] = - D/dx**2; A[n,n-1] = 2*D/dx**2; A[n,n-2] = - D/dx**2#A[n,n-2] = 1;
 b[n] = s 
 Phi = np.zeros([itr,itr_m])
-#Phi[0,] = 0
-#f = 0
 
 # Using a 1D Analysis in the x direction
 # want to solve for vectore phi
@@ -49,25 +34,6 @@ for j in np.arange(1,m+1,1):
         A[i,i+1] =  u/(2*dx) - D/dx**2 
     phi = np.linalg.solve(A,b)
     Phi[:,j] = phi 
-    
-#A[0,0] = 1; b[0] = 0 #A[0,1] = - 2*D/dx**2; A[0,2] = - u/(2*dx) - D/dx**2 ;
-#A[n,n] = - D/dx**2; A[n,n-1] = 2*D/dx**2; A[n,n-2] = - D/dx**2#A[n,n-2] = 1;
-#b[n] = s 
-#phi = np.linalg.solve(A,b)
-
-#for j in np.arange(1,n,1):
-#for i in np.arange(1,n,1):
-#    b[i-1,i] = s + 1/dt
-#    b[i,i] = s - 1/dt
-#    A[:,i,i-1] = - u/(2*dx) - D/dx**2        
-#    A[:,i,i] = 2*D/dx**2  
-#    A[:,i,i+1] =  u/(2*dx) - D/dx**2 
-
-#    
-#A[:,0,0] = 1; b[:,0] = 0 #A[0,1] = - 2*D/dx**2; A[0,2] = - u/(2*dx) - D/dx**2 ;
-##A[n,n] = 3; A[n,n-1] = -4; A[n,n-2] = 1;
-#A[:,n,n] = - D/dx**2; A[:,n,n-1] = 2*D/dx**2; A[:,n,n-2] = - D/dx**2#A[n,n-2] = 1;
-#b[:,n] = s #2/n**2
 
 plt.figure(1,figsize=(6, 4),dpi=50)
 plt.plot(x,Phi[:,itr_m-2], label='phi')
@@ -75,15 +41,69 @@ plt.plot(x,Phi[:,itr_m-2], label='phi')
 plt.title("Phi")
 plt.xlabel('x_n')
 plt.ylabel('Phi(x_n)')
-#plt.legend()
+
+V = np.ones(itr)/n
+width = V[1]
+edge = np.linspace(0., 1., num=itr) # edges of segments
+center = np.linspace(0, 1., num=itr) + 1/2/n # edges of segments
+center = center[:-1]
+phi1 = np.zeros([n])
+A1 = np.zeros([n,n])
+b1 = np.zeros([n])
+Phi1 = np.zeros([itr-1,itr_m-1])
+
+c2f_forward = edge[2] - center[1]
+c2f_backward = center[1] - edge[1]
+c2c_forward =  center[2] - center[1]
+c2c_backward = center[1] - center[0]
+
+A1[0,0] = 1; b1[0] = 0 
+A1[n-1,n-1] = - D/c2c_forward; A1[n-1,n-2] = 2*D/c2c_backward; A1[n-1,n-3] = -D/c2c_backward;
+b1[n-1] = V[n-1] 
+
+# Using a 1D Analysis in the x direction
+# want to solve for vectore phi
+for j in np.arange(1,m,1):
+    for i in np.arange(1,n-1,1):
+        b1[i] = V[i] + Phi[i,j-1]/dt # how do I change these equations
+        c2f_forward = edge[i+1] - center[i]
+        c2f_backward = center[i] - edge[i]
+        c2c_forward =  center[i+1] - center[i]
+        c2c_backward = center[i] - center[i-1]
+        A1[i,i-1] = (-u*(c2f_backward/c2c_backward) - D/c2c_backward)      
+        A1[i,i] = (2*D/c2c_backward) + 1/dt 
+        A1[i,i+1] = (u*(c2f_forward/c2c_forward) - D/c2c_forward)
+    phi1 = np.linalg.solve(A1,b1)
+    Phi1[:,j] = phi1 
+    
+plt.figure(2,figsize=(6, 4),dpi=50)
+plt.plot(center,Phi1[:,itr_m-2], label='phi')
+#plt.ylim([-2, 2])
+plt.title("Phi")
+plt.xlabel('midpoint of segment')
+plt.ylabel('Phi(x_n)')
+#plt.legend(['Finite Diff','Finite Volume'])
 #plt.show()
 
-
-#plt.figure(1,figsize=(6, 4),dpi=50)
-#plt.plot(center,phi1, label='phi')
+# what do I do which j=1?
+for j in np.arange(2,m,1):
+    for i in np.arange(1,n-1,1):
+        b1[i] = V[i] + 4*Phi[i,j-1]/dt/2 - Phi[i,j-2]/dt/2 # how do I change these equations
+        c2f_forward = edge[i+1] - center[i]
+        c2f_backward = center[i] - edge[i]
+        c2c_forward =  center[i+1] - center[i]
+        c2c_backward = center[i] - center[i-1]
+        A1[i,i-1] = (-u*(c2f_backward/c2c_backward) - D/c2c_backward)      
+        A1[i,i] = (2*D/c2c_backward) + 3/dt/2 
+        A1[i,i+1] = (u*(c2f_forward/c2c_forward) - D/c2c_forward)
+    phi1 = np.linalg.solve(A1,b1)
+    Phi1[:,j] = phi1 
+    
+plt.figure(3,figsize=(6, 4),dpi=50)
+plt.plot(center,Phi1[:,itr_m-2], label='phi')
 #plt.ylim([-2, 2])
-#plt.title("Phi")
-#plt.xlabel('midpoint of segment')
-#plt.ylabel('Phi(x_n)')
-#plt.legend()
+plt.title("Phi")
+plt.xlabel('midpoint of segment')
+plt.ylabel('Phi(x_n)')
+#plt.legend(['Finite Diff','Finite Volume'])
 #plt.show()
--- HW9/FMB_p1.py
+++ HW9/FMB_p1.py
@@ -21,10 +21,6 @@ phi = np.zeros([itr])
 A = np.zeros([itr,itr])
 b = np.zeros([itr])
 dx = x[1]
-#phi = np.linspace(0., 1., num=itr) # want to solve for phi using linear equation A*phi = b
-# what is A and b?
-
-#s = phi_d + np.matmul(Delta,(np.matmul(u,phi_bar) - Delta_phi_bar))
 
 # Using a 1D Analysis in the x direction
 # want to solve for vectore phi
@@ -37,7 +33,7 @@ for i in np.arange(1,n,1):
     A[i,i+1] =  u/(2*dx) - D/dx**2 
     
 A[0,0] = 1; b[0] = 0 
-A[n,n] = - D/dx**2; A[n,n-1] = 2*D/dx**2; A[n,n-2] = - D/dx**2#A[n,n-2] = 1;
+A[n,n] = - D/dx**2; A[n,n-1] = 2*D/dx**2; A[n,n-2] = - D/dx**2
 b[n] = s 
 
 phi = np.linalg.solve(A,b)
@@ -53,17 +49,14 @@ center = np.linspace(0, 1., num=itr) + 1/2/n # edges of segments
 center = center[:-1]
 phi1 = np.zeros([n])
 A1 = np.zeros([n,n])
-#c2f = np.zeros([n,n])
-#c2c = np.zeros([n,n])
 b1 = np.zeros([n])
-#phi = np.linspace(0., 1., num=itr) # want to solve for phi using linear equation A*phi = b
 
 # Using a 1D Analysis in the x direction
 # want to solve for vectore phi
 
 for i in np.arange(1,n-1,1):
 #    print(i)
-    b1[i] = V[i] # magnitude of Vi multiplied by si(t); volume in this case is time invariant
+    b1[i] = V[i] 
     c2f_forward = edge[i+1] - center[i]
     c2f_backward = center[i] - edge[i]
     c2c_forward =  center[i+1] - center[i]
@@ -73,7 +66,7 @@ for i in np.arange(1,n-1,1):
     A1[i,i+1] = (u*(c2f_forward/c2c_forward) - D/c2c_forward)
     
 A1[0,0] = 1; b1[0] = 0 
-A1[n-1,n-1] = - D/c2c_forward; A1[n-1,n-2] = 2*D/c2c_backward; A1[n-1,n-3] = -D/c2c_backward;#A[n,n-2] = 1;
+A1[n-1,n-1] = - D/c2c_forward; A1[n-1,n-2] = 2*D/c2c_backward; A1[n-1,n-3] = -D/c2c_backward;
 b1[n-1] = V[n-1] 
 
 phi1 = np.linalg.solve(A1,b1)
