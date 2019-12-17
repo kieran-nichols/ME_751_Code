74358ba2012099e612c18cfb6b91e8dfe78f3db5
the1kieran <knichols4@wisc.edu>
2019-12-16 22:52:36 -0600

    added new env of chronoant to play with cost functions

--- chrono-tensorflow/envs/chtrain.py
+++ chrono-tensorflow/envs/chtrain.py
@@ -6,12 +6,16 @@ Created on Thu Jan 10 11:01:21 2019
 """
 
 import chtrain_ant
+import chtrain_ant_v1
 import chtrain_pendulum
 import chtrain_humanoid
        
 def Init(env_name, render):
        if env_name=='ChronoAnt':
               return chtrain_ant.Model(render)
+
+       elif env_name=='ChronoAntv1':
+              return chtrain_ant_v1.Model(render)
                      
        elif env_name=='ChronoPendulum':
               return chtrain_pendulum.Model(render)
